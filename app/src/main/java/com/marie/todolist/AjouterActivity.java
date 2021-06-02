78891305a87633435eaf893c5594e928070e0f81
marieThielens <thielens.marie@gmail.com>
2021-06-02 02:13:17 -0700

    upgrade

--- app/src/main/java/com/marie/todolist/AjouterActivity.java
+++ app/src/main/java/com/marie/todolist/AjouterActivity.java
@@ -6,6 +6,7 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -18,15 +19,19 @@ import com.marie.todolist.db.TodoStructureDB;
 import com.marie.todolist.enums.PrioriteEnum;
 import com.marie.todolist.models.Tache;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class AjouterActivity extends AppCompatActivity {
 
     private Button btnRetourAccueil, btnValiderTache;
     private EditText inputNomTache, inputImportanceTache, inputAjouterDate;
     private ImageView btnCalendar;
+    private LocalDate dateLimite = null;
     Spinner spPriorite;
 
     @Override
@@ -44,16 +49,33 @@ public class AjouterActivity extends AppCompatActivity {
 
 
         // le calendrier  ..............
+        // Désactivation de la saisie via le clavier
+        inputAjouterDate.setInputType(InputType.TYPE_NULL);
+
+        Calendar calendar = Calendar.getInstance();
+        int jour = calendar.get(Calendar.DAY_OF_MONTH);
+        int mois = calendar.get(Calendar.MONTH);
+        int annee = calendar.get(Calendar.YEAR);
+
         btnCalendar.setOnClickListener(v -> {
             DatePickerDialog picker = new DatePickerDialog(AjouterActivity.this,
                     new DatePickerDialog.OnDateSetListener() { // l'utilisateur à cliqué sur une date
                         @Override
                         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                            inputAjouterDate.setText(String.format("´%s %s %S" , dayOfMonth ,  month , year ));
+
+                            // je récupère le contexte de l'pp > puis les ressources > la configuration > locale (langue utilisé) > la 1ere
+                            Locale locale = AjouterActivity.this.getResources().getConfiguration().getLocales().get(0) ;
+
+                            dateLimite = LocalDate.of(year, month + 1, dayOfMonth);
+                            String date = dateLimite.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.FRENCH));
+                            inputAjouterDate.setText(date);
+
+                            //inputAjouterDate.setText(String.format("´%s %s %S" , jour ,  mois , annee ));
                         }
                     },
-                    Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH) );
-                 picker.show();
+                    annee, mois, jour );
+
+            picker.show();
         });
 
 
